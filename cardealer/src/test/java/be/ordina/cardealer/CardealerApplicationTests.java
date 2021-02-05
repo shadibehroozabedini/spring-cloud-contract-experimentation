49d5c7d7f69e91ca1cda4c6fdc0fb3ad4052df21
shadiabedini <shadi.behroozabedini@ordina.be>
2021-02-05 10:35:14 +0100

    add cardealer app

---
+++ cardealer/src/test/java/be/ordina/cardealer/CardealerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ordina.cardealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardealerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
