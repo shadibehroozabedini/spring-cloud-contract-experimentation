f25db8dcfdf614e8f07ae48630fd44058c6d3476
shadiabedini <shadi.behroozabedini@ordina.be>
2021-02-05 10:42:13 +0100

    add carfactory app

---
+++ carfactory/src/main/java/be/ordina/shbe/carfactory/CarfactoryApplication.java
@@ -0,0 +1,13 @@
+package be.ordina.shbe.carfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarfactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarfactoryApplication.class, args);
+	}
+
+}
